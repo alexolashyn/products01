731fbc97b5f6aed6698a9727d6bfc415f93a2e1a
sashka.olashyn <sashka.olashyn@gmail.com>
2022-04-07 16:16:46 +0300

    progress

--- main.js
+++ main.js
@@ -9,6 +9,30 @@ const functions = require('./functions');
 const {getProducts} = require('./product');
 
 const products = getProducts();
-
-console.log(products[0]);
+function  logProductsWithDiscount() {
+    for(let p of products){
+        p.flag=functions.getFlag(p.prices[p.prices.length-2], p.prices[p.prices.length-1]);
+    }
+    const productsForNotifications=[];
+    for(let p of products){
+        switch(p.flag){
+            case FLAG_PRICE_HUGE_DECREASE:
+                productsForNotifications.push(p);
+                break;
+            case FLAG_PRICE_DECREASE:
+                productsForNotifications.push(p);
+                break;
+            case FLAG_PRICE_INCREASE:
+                break;
+            case FLAG_ERROR:
+                break;
+            case NO_FLAG:
+                break;
+        }
+    }
+    for(let i=0;i<productsForNotifications.length;i+=1){
+        console.log(productsForNotifications[i]);
+    }
+}
+logProductsWithDiscount();
 
